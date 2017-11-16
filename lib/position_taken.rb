8ac3309e0376af92aa7dc6496fd3202452b5bf35
Reno Sorensen <renojames19@gmail.com>
2017-11-16 02:42:22 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if index == "O" || index = "X"
     return true
-  elsif index == " " || or index == ""
+  elsif index == " " || index == ""
     return false
   end
 end
