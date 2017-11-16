019bd753a55305fb60d2aa083d769bf7453c30bd
Reno Sorensen <renojames19@gmail.com>
2017-11-16 02:31:50 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if index = " "
+  if index == " "
     return false
-  elsif index = "O" || index = "X"
+  elsif index == "O" || index = "X"
     return true
   end
 end
